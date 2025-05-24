afc66850f57bf912e085cc8c4055c91c3e93b719
ltf2323361 <kayalutfullah195@gmail.com>
2025-05-24 11:20:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -464,5 +464,6 @@ def favori_cikar(arac_id):
 def favori_mi(user_id, arac_id):
     return Favori.query.filter_by(user_id=user_id, arac_id=arac_id).first() is not None
 
+import os
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
